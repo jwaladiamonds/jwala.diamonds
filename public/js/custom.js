ad58b8a2451618c6737913378588699a28161f92
Nikhil John <nikhiljohn1010@gmail.com>
2020-08-07 16:50:13 +0530

    Optimization Phase 2 BugFix 01

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,4 +1,3 @@
-
 if ('loading' in HTMLImageElement.prototype) {
   const lazyImages = document.querySelectorAll('img[loading="lazy"]')
   lazyImages.forEach(img => {
@@ -9,49 +8,67 @@ if ('loading' in HTMLImageElement.prototype) {
   })
 } else {
   const script = document.createElement('script')
-  script.src = 'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.1.2/lazysizes.min.js'
+  script.src = 'public/js/lazysizes.min.js'
   document.body.appendChild(script)
 }
 
+let imagesLoaded = 0, thumbsLoaded = 0
 
-// Preloading
-let img, wow, imagesLoaded = 0, imagesLoadedinCarousel = 0
-images = document.querySelectorAll('img.wait')
-images.forEach((item, id) => {
-  let image = new Image()
-  image.src = item.src
-  image.onload = function(e) {
-    imagesLoaded++
-    if (imagesLoaded === images.length) {
-      $(".preloader").delay(400).fadeOut(800);
-    }
+class ImageLoader {
+  constructor() {
+    this.list = {}
+    this.waitList = document.querySelectorAll('img.wait')
   }
-})
 
-animages = document.querySelectorAll('img.fadeIn')
-animages.forEach((item, id) => {
-  animages[id].onload = function(e) {
-    animages[id].classList.remove('fadeIn')
+  loadImages(key, nodeList) {
+    this.list[key] = nodeList
   }
-})
 
+  waitToLoad(callback) {
+    const count = this.waitList.length
+    this.waitList.forEach((node, id) => {
+      let image = new Image()
+      if ("src" in node.dataset) image.src = node.dataset.src
+      else image.src = node.src
+      image.onload = function(e) {
+        callback(node, count)
+      }
+    })
+  }
 
-carouselThumbs = document.querySelectorAll('.carousel-indicators img')
-carouselFirstImage = document.querySelectorAll('.carousel-inner img')[0]
-carouselFirstImage.onload = function(e) {
-  imagesLoadedinCarousel++
-  if (imagesLoadedinCarousel === carouselThumbs.length+1) {
-    document.querySelector('.carousel.fadeIn').classList.remove('fadeIn')
+  onLoad(key, callback) {
+    const count = this.list[key].length
+    this.list[key].forEach((node, id) => {
+      node.onload = function(e) {
+        callback(node, count)
+      }
+    })
   }
 }
-carouselThumbs.forEach((item, id) => {
-  let cimage = new Image()
-  cimage.src = item.dataset.src
-  cimage.onload = function(e) {
-    imagesLoadedinCarousel++
-    if (imagesLoadedinCarousel === carouselThumbs.length+1) {
-      document.querySelector('.carousel.fadeIn').classList.remove('fadeIn')
-    }
+
+
+imageLoader = new ImageLoader()
+imageLoader.loadImages('fadeIn', document.querySelectorAll('img.fadeIn'))
+imageLoader.loadImages('thumbs', document.querySelectorAll('.carousel-indicators img'))
+
+// Preloading
+imageLoader.waitToLoad((image, count) => {
+  imagesLoaded++
+  if (imagesLoaded === count) {
+    $(".preloader").delay(400).fadeOut(800);
+  }
+})
+
+// FadeIn images
+imageLoader.onLoad('fadeIn',(image, count) => {
+  image.classList.remove('fadeIn')
+})
+
+// FadeIn Carousel
+imageLoader.onLoad('thumbs',(image, count) => {
+  thumbsLoaded++
+  if (thumbsLoaded === count) {
+    document.querySelector('.carousel.fadeIn').classList.remove('fadeIn')
   }
 })
 
@@ -78,19 +95,19 @@ $(document).ready(function() {
 
   // Parallax Effect
   jarallax(document.querySelectorAll('.jarallax'));
-  jarallax(document.querySelectorAll('.jarallax-keep-img'), { keepImg: true });
+  jarallax(document.querySelectorAll('.jarallax-keep-img'), {
+    keepImg: true
+  });
 
   // Google Map
-  $('#map-container-google').html('<iframe title="Google Map of Jwala Diamonds" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDNLVEUc-pMs2303wH9hOyzuc8qzccwnZg&q=Jwala%20Diamonds%20and%20Jewellery,Thrissur&region=IN" frameborder="0" style="border:0" allowfullscreen></iframe>')
+  //$('#map-container-google').html('<iframe title="Google Map of Jwala Diamonds" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDNLVEUc-pMs2303wH9hOyzuc8qzccwnZg&q=Jwala%20Diamonds%20and%20Jewellery,Thrissur&region=IN" frameborder="0" style="border:0" allowfullscreen></iframe>')
 })
-
-//*/
 
 document.onreadystatechange = function() {
   if (document.readyState === "complete") {
     const pswpElement = document.querySelector('.pswp')
     const options = {
-      preload: [1,3],
+      preload: [1, 3],
       showHideOpacity: true
     }
 
